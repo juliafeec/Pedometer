f93de263323f0e86f3dc6c47e8bd098ea206f799
Julia <julia.feec@gmail.com>
2012-04-26 19:21:51 +0200

    fromeclipse

---
+++ Pedometer/src/com/pedometer/PedometerActivity.java
@@ -0,0 +1,13 @@
+package com.pedometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PedometerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
